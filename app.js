8aa254c550e8d16e67e6472eb9afa319f2e5ba2e
vamsee <vamsee.somasi@gmail.com>
2022-02-15 23:57:10 +0530

    Add GET method to fetch user's data. Fix database name since it already exists.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-//creating USERDB to store the data
-mongoose.connect("mongodb://localhost:27017/USERDB",{useNewUrlParser: true});
+//creating regDB to store the data
+mongoose.connect("mongodb://localhost:27017/regDB",{useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -25,7 +25,38 @@ app.post("/register", function(req, res){
     const password = req.body.password;
     // enter dob as yyyy-mm-dd(BSON model).
     const dob = new Date(req.body.dob);
-    console.log("Age is: ", 2022 - dob.getFullYear());
+    if(2022 - dob.getFullYear() <= 14){
+        res.send("Age should be above 14 years!");
+    } else {
+        const newUser = new User({
+            name: name,
+            email: email,
+            password: password,
+            dob: dob
+        });
+        newUser.save(function(err){
+            if(err){
+                res.send("Error while saving: " + err);
+            } else {
+                res.send("User has been registered successfully");
+            }
+        });
+    }
+});
+
+//GET method for fetching user data.
+app.get("/userData", function(req, res){
+    User.find({},function(err, foundUsers){
+        if(err){
+            res.send(err);
+        } else {
+            if(foundUsers){
+                res.send(foundUsers);
+            } else {
+                res.send("No users found");
+            }
+        }
+    });
 });
 
 app.listen(8080, function(){
